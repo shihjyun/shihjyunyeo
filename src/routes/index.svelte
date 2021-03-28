4fc9c753dda7d3f776b4723b5dcbdac9f97c8469
Steven Yeo <shihjyun.yeo@gmail.com>
2021-03-29 00:09:00 +0800

    add simple landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,37 @@
-<script>
-	import Counter from '$lib/Counter.svelte';
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('/assets/index/base_data/shihjyun_data.json')
+    const shihjyun = await res.json()
+
+    return { props: { shihjyun } }
+  }
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<script>
+  import Intro from '$lib/index/Intro.svelte'
+  export let shihjyun
 
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
+  console.log(shihjyun)
+</script>
 
 <style>
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
+  section.intro {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 800px;
+    margin: 0 auto;
+  }
 </style>
+
+<svelte:head>
+  <title>Steven Yeo</title>
+  <!-- <meta name="description" content="" /> -->
+</svelte:head>
+
+<section class="intro">
+  <Intro briefIntro={shihjyun.brief_intro} />
+</section>
+
+<section />
+<main />
