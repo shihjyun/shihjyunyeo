97c596f03708e4ffed185b19ed64d2cd4e8be691
Steven Yeo <shihjyun.yeo@gmail.com>
2022-02-10 22:47:13 +0800

    add post's metadata

--- src/routes/post/[slug].svelte
+++ src/routes/post/[slug].svelte
@@ -24,11 +24,26 @@
   export let url;
   export let post;
 
+  $: console.log(post);
+
   import { setContext } from 'svelte';
 
   import Post from '$lib/post/Post.svelte';
 
   setContext('url', url);
 </script>
+
+<svelte:head>
+  <title>{`${post.title} - Steven Yeo`}</title>
+  <meta property="og:url" content={url} />
+  <meta content={`${post.title} - Steven Yeo`} property="og:title">
+  <meta content={post.description} name="description">
+  <meta content="article" property="og:type">
+  <meta content={`${url}/${post.og_mage}`} property="og:image">
+  <meta name="twitter:title" content={`${post.title} - Steven Yeo`}>
+  <meta name="twitter:description" content={post.description}>
+  <meta name="twitter:image" content={`${url}/${post.og_mage}`}>
+  <meta name="twitter:card" content="summary_large_image">
+</svelte:head>
 
 <Post {post} />
