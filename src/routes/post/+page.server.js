89d0b7a4f4447031f62a9fb86d5d401a48bc9844
Steven Yeo <shihjyun.yeo@cw.com.tw>
2025-02-03 01:21:50 +0800

    remove redundant files

--- src/routes/post/+page.server.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageServerLoad} */
-
-export async function load({ url }) {
-  const posts = await fetch(`${url.origin}/post/post-meta.json`);
-
-  return { posts: await posts.json() };
-}
--- src/routes/works/+page.server.js
+++
@@ -1,9 +0,0 @@
-import archieml from 'archieml';
-
-/** @type {import('./$types').PageServerLoad} */
-
-export async function load({ url }) {
-  const works = await fetch(`${url.origin}/works/works-meta.txt`);
-
-  return { works:  archieml.load(await works.text()) };
-}
